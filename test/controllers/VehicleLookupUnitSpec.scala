91152efa6cbf05a460680f9303530b611747606e
Scott Walker <scott.walker@valtech.co.uk>
2014-11-19 15:15:58 +0000

    Fixing tests for VehicleLookup

--- test/controllers/VehicleLookupUnitSpec.scala
+++ test/controllers/VehicleLookupUnitSpec.scala
@@ -20,6 +20,7 @@ import uk.gov.dvla.vehicles.presentation.common.services.DateService
 import views.vrm_assign.Payment._
 import views.vrm_assign.VehicleLookup._
 import webserviceclients.fakes.AddressLookupServiceConstants.PostcodeValid
+import webserviceclients.fakes.BruteForcePreventionWebServiceConstants
 import webserviceclients.fakes.BruteForcePreventionWebServiceConstants.VrmLocked
 import webserviceclients.fakes.VehicleAndKeeperLookupWebServiceConstants._
 import org.mockito.Matchers.any
@@ -251,16 +252,15 @@ final class VehicleLookupUnitSpec extends UnitSpec {
       }
     }
 
-//    "redirect to VehicleAndKeeperLookupFailure and display 2nd attempt message when document reference number not found and security service returns 2nd attempt" in new WithApplication {
-//      val request = buildCorrectlyPopulatedRequest(registrationNumber = VrmAttempt2)
-//      val result = vehicleLookupStubs(
-//        vehicleDetailsResponseDocRefNumberNotLatest,
-//        bruteForceService = bruteForceServiceImpl(permitted = true)
-//      ).submit(request)
-//
-//      result.futureValue.header.headers.get(LOCATION) should equal(Some(VehicleAndKeeperLookupFailurePage.address))
-//    }
-//
+    "redirect to VehicleAndKeeperLookupFailure and display 2nd attempt message when document reference number not found and security service returns 2nd attempt" in new WithApplication {
+      val request = buildCorrectlyPopulatedRequest(registrationNumber = BruteForcePreventionWebServiceConstants.VrmAttempt2)
+      val result = vehicleLookupStubs(
+        vehicleAndKeeperLookupStatusAndResponse = vehicleAndKeeperDetailsResponseDocRefNumberNotLatest
+      ).submit(request)
+
+      result.futureValue.header.headers.get(LOCATION) should equal(Some(VehicleLookupFailurePage.address))
+    }
+
 //    "Send a request and a trackingId" in new WithApplication {
 //      val trackingId = "x" * 20
 //      val request = buildCorrectlyPopulatedRequest().
