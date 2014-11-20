da4edde42fb7f8f15cc16f67fcc3c05206191952
Geraint Jones <geraint.jones@valtech.co.uk>
2014-11-20 10:48:41 +0000

    change to vehicle lookup failure to handle cert number mismatch

--- app/views/vrm_assign/VehicleLookupFailure.scala
+++ app/views/vrm_assign/VehicleLookupFailure.scala
@@ -3,6 +3,7 @@ package views.vrm_assign
 object VehicleLookupFailure {
 
   final val VehicleLookupId = "vehicle-lookup"
+  final val CaptureCertificateDetailsId = "capture-certificate-details"
   final val BeforeYouStartId = "before-you-start"
   final val ExitId = "exit"
 }
