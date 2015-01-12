020927ff5aa02cb17411558e8c620b982ec0d792
Ian Stainer <ian.stainer@valtech.co.uk>
2015-01-12 16:47:39 +0000

    US2201 - prefixed the cache keys with exemplar abbreviation to make the keys unique across all exemplars which will now use a common domain name

--- app/models/package.scala
+++ app/models/package.scala
@@ -1,4 +1,7 @@
 package object models {
 
+  final val CacheKeyPrefix = "asn-"
+  final val PrScopedCacheKeyPrefix = "ret-asn-"
+
   final val SeenCookieMessageCacheKey = "seen_cookie_message"
 }
--- app/views/vrm_assign/EnterAddressManually.scala
+++ app/views/vrm_assign/EnterAddressManually.scala
@@ -1,8 +1,10 @@
 package views.vrm_assign
 
+import models.CacheKeyPrefix
+
 object EnterAddressManually {
 
-  final val EnterAddressManuallyCacheKey = "asn-enter-address-manually"
+  final val EnterAddressManuallyCacheKey = s"${CacheKeyPrefix}enter-address-manually"
   final val PostcodeId = "postcode"
   final val NextId = "next"
   final val ExitId = "exit"
