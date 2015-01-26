e3c93de1cefd911e19020f5b9bb87ba7f15b4680
Scott Walker <scott.walker@valtech.co.uk>
2015-01-26 09:48:12 +0000

    US2177: event tracking javascript now has specialised methods that take in arrays of ids to cut down on the amount of lines we are writing (no longer 1 line per id)

--- app/assets/javascripts/google-analytics-event-tracking.js
+++ app/assets/javascripts/google-analytics-event-tracking.js
@@ -1,7 +1,9 @@
 /*
+* Add an event listener to a html element.
+*
 * inputId - the id of the html field you want to track.
 *
-* event - A String that specifies the name of the event.
+* event - A String that specifies the name of the event e.g. 'click', 'change', etc.
 * Note: Do not use the "on" prefix. For example, use "click" instead of "onclick".
 * For a list of all HTML DOM events, look at our complete HTML DOM Event Object Reference http://www.w3schools.com/jsref/dom_obj_event.asp.
 */
@@ -27,5 +29,29 @@ function track(inputId, event) {
     } else {
         console.error("element id: " + inputId + " not found on page");
         return false;
+    }
+}
+
+/*
+* Add a listener that fires when the link or button is clicked.
+*
+* ids - The ids of the links/buttons you want to track.
+*/
+function trackClick(ids) {
+    for (var i = 0; i < ids.length; i++) {
+        track(ids[i], 'click');
+    }
+}
+
+/*
+* Add a listener that fires when the user leaves the text field (e.g by tabbing out of it) and the text is different to
+* what was there before.
+*
+* ids - The ids of the text fields you want to track.
+*/
+function trackChange(ids) {
+    for (var i = 0; i < ids.length; i++) {
+        console.log("attach listener to: " + ids[i]);
+        track(ids[i], 'change');
     }
 }
