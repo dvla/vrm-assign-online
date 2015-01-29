30ba812a5c594750bd2b3cf5c70d68ca08d86580
Scott Walker <scott.walker@valtech.co.uk>
2015-01-29 16:07:20 +0000

    US2177: Binding for AuditService 2

--- test/composition/TestComposition.scala
+++ test/composition/TestComposition.scala
@@ -5,6 +5,7 @@ import com.google.inject.{Guice, Injector, Module}
 import com.tzavellas.sse.guice.ScalaModule
 import composition.audit1.AuditLocalServiceDoesNothingBinding
 import composition.webserviceclients.addresslookup.AddressLookupServiceBinding
+import composition.webserviceclients.audit2.AuditServiceDoesNothing
 import composition.webserviceclients.bruteforceprevention.{TestBruteForcePreventionWebServiceBinding, BruteForcePreventionServiceBinding}
 import composition.webserviceclients.paymentsolve.{PaymentServiceBinding, TestPaymentWebServiceBinding}
 import composition.webserviceclients.vehicleandkeeperlookup.{TestVehicleAndKeeperLookupWebServiceBinding, VehicleAndKeeperLookupServiceBinding}
@@ -32,7 +33,6 @@ trait TestComposition extends Composition {
       new LoggerLikeBinding,
       new PdfServiceBinding,
       new EmailServiceBinding,
-      new AuditLocalServiceDoesNothingBinding,
       // Completely mocked web services below...
       new TestConfig,
       new TestAddressLookupWebServiceBinding,
@@ -42,7 +42,9 @@ trait TestComposition extends Composition {
       //  VrmAssignFulfilWebService, // TODO there should be a stubbed version of this web service!
       new TestPaymentWebServiceBinding,
       new TestBruteForcePreventionWebServiceBinding,
-      new TestRefererFromHeaderBinding
+      new TestRefererFromHeaderBinding,
+      new AuditLocalServiceDoesNothingBinding,
+      new AuditServiceDoesNothing
     ).`with`(modules: _*)
     Guice.createInjector(overriddenDevModule)
   }
@@ -52,6 +54,5 @@ final class TestModule extends ScalaModule with MockitoSugar {
 
   def configure() {
     bind[_root_.webserviceclients.audit2.AuditMicroService].to[_root_.webserviceclients.audit2.AuditMicroServiceImpl].asEagerSingleton()
-    bind[_root_.webserviceclients.audit2.AuditService].toInstance(new composition.webserviceclients.audit2.AuditServiceDoesNothing().build())
   }
 }
