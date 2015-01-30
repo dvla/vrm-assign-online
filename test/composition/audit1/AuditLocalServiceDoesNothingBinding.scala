3a51ad823e07c74e968f594d8c4a7541b74f6294
Scott Walker <scott.walker@valtech.co.uk>
2015-01-30 12:40:59 +0000

    US2177: remove DI mocks from IoC module

--- test/composition/audit1/AuditLocalServiceDoesNothingBinding.scala
+++ test/composition/audit1/AuditLocalServiceDoesNothingBinding.scala
@@ -2,16 +2,15 @@ package composition.audit1
 
 import com.tzavellas.sse.guice.ScalaModule
 import org.mockito.Matchers.any
-import org.mockito.Mockito.{mock, when}
+import org.mockito.Mockito._
 import org.mockito.internal.stubbing.answers.DoesNothing
 import org.scalatest.mock.MockitoSugar
 import uk.gov.dvla.auditing.Message
 
-final class AuditLocalServiceDoesNothingBinding(
-                                                 auditService1: audit1.AuditService = mock(classOf[audit1.AuditService])
-                                                 ) extends ScalaModule with MockitoSugar {
+final class AuditLocalServiceDoesNothingBinding extends ScalaModule with MockitoSugar {
 
   val stub = {
+    val auditService1: audit1.AuditService = mock[audit1.AuditService]
     when(auditService1.send(any[Message])).thenAnswer(new DoesNothing)
     auditService1
   }
