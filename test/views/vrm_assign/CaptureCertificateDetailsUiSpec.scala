9556890d2e55a807403e638fb40916ab1b5cac03
Scott Walker <scott.walker@valtech.co.uk>
2015-02-24 11:33:56 +0000

    US2368: trying to write a test for auto-tab, but cannot get normal test to submit correctly

--- test/views/vrm_assign/CaptureCertificateDetailsUiSpec.scala
+++ test/views/vrm_assign/CaptureCertificateDetailsUiSpec.scala
@@ -9,8 +9,12 @@ import org.openqa.selenium.WebDriver
 import org.openqa.selenium.WebElement
 import org.scalatest.selenium.WebBrowser._
 import pages.common.MainPanel.back
+import pages.vrm_assign.CaptureCertificateDetailsPage.date
+import pages.vrm_assign.CaptureCertificateDetailsPage.documentCount
+import pages.vrm_assign.CaptureCertificateDetailsPage.registrationMark
+import pages.vrm_assign.CaptureCertificateDetailsPage.time
+import pages.vrm_assign.CaptureCertificateDetailsPage.prVrm
 import pages.vrm_assign._
-import views.vrm_assign.Confirm.ConfirmCacheKey
 
 final class CaptureCertificateDetailsUiSpec extends UiSpec with TestHarness {
 
@@ -33,7 +37,49 @@ final class CaptureCertificateDetailsUiSpec extends UiSpec with TestHarness {
       csrf.getAttribute("name") should equal(uk.gov.dvla.vehicles.presentation.common.filters.CsrfPreventionAction.TokenName)
       csrf.getAttribute("value").size > 0 should equal(true)
     }
+  }
 
+//  "confirm button" should {
+//
+//    "redirect to confirm page when next button is clicked" taggedAs UiTag in new WebBrowserForSelenium {
+//      go to BeforeYouStartPage
+//      cacheSetup()
+//      go to CaptureCertificateDetailsPage
+//      documentCount.value = "1"
+//      date.value = "11111"
+//      time.value = "111111"
+//      registrationMark.value = "11111111"
+//      prVrm.value = "A1"
+//
+//      click on CaptureCertificateDetailsPage.lookup
+//
+//      currentUrl should equal(ConfirmPage.url)
+//    }
+//  }
+
+  "exit" should {
+    "display feedback page when exit link is clicked" taggedAs UiTag in new WebBrowserForSelenium {
+      go to BeforeYouStartPage
+      cacheSetup()
+      go to CaptureCertificateDetailsPage
+
+      click on CaptureCertificateDetailsPage.exit
+
+      currentUrl should equal(LeaveFeedbackPage.url)
+    }
+  }
+
+  "back button" should {
+
+    "redirect to SetUpBusinessDetails page" taggedAs UiTag in new WebBrowserForSelenium {
+      go to BeforeYouStartPage
+      cacheSetup()
+      go to CaptureCertificateDetailsPage
+
+      click on back
+
+      currentUrl should equal(VehicleLookupPage.url)
+    }
   }
 
   private def cacheSetup()(implicit webDriver: WebDriver) =
