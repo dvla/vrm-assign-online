17e390628019d400014e5c4a52e5efea355dd730
Scott Walker <scott.walker@valtech.co.uk>
2015-02-24 11:10:31 +0000

    Adding missing Ui spec

---
+++ test/views/vrm_assign/CaptureCertificateDetailsUiSpec.scala
@@ -0,0 +1,45 @@
+package views.vrm_assign
+
+import composition.TestHarness
+import helpers.UiSpec
+import helpers.tags.UiTag
+import helpers.vrm_assign.CookieFactoryForUISpecs
+import org.openqa.selenium.By
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.WebElement
+import org.scalatest.selenium.WebBrowser._
+import pages.common.MainPanel.back
+import pages.vrm_assign._
+import views.vrm_assign.Confirm.ConfirmCacheKey
+
+final class CaptureCertificateDetailsUiSpec extends UiSpec with TestHarness {
+
+  "go to page" should {
+
+    "display the page" taggedAs UiTag in new WebBrowserForSelenium {
+      go to BeforeYouStartPage
+
+      cacheSetup()
+
+      go to CaptureCertificateDetailsPage
+
+      currentUrl should equal(CaptureCertificateDetailsPage.url)
+    }
+
+    "contain the hidden csrfToken field" taggedAs UiTag in new WebBrowserForSelenium {
+      go to CaptureCertificateDetailsPage
+      val csrf: WebElement = webDriver.findElement(By.name(uk.gov.dvla.vehicles.presentation.common.filters.CsrfPreventionAction.TokenName))
+      csrf.getAttribute("type") should equal("hidden")
+      csrf.getAttribute("name") should equal(uk.gov.dvla.vehicles.presentation.common.filters.CsrfPreventionAction.TokenName)
+      csrf.getAttribute("value").size > 0 should equal(true)
+    }
+
+  }
+
+  private def cacheSetup()(implicit webDriver: WebDriver) =
+    CookieFactoryForUISpecs.
+      vehicleAndKeeperLookupFormModel().
+      vehicleAndKeeperDetailsModel().
+      businessDetails().
+      transactionId()
+}
