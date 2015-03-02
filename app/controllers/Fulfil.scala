e577b367e472d35300b166694d21474c3dcda3f5
Geraint Jones <geraint.jones@valtech.co.uk>
2015-03-02 12:58:07 +0000

    DE353 add the capture cert form and PACT data to PaymentToPaymentFailure

--- app/controllers/Fulfil.scala
+++ app/controllers/Fulfil.scala
@@ -146,6 +146,8 @@ final class Fulfil @Inject()(
 
       // TODO need to tidy this up!!
       val paymentModel = request.cookies.getModel[PaymentModel]
+      val captureCertificateDetailsFormModel = request.cookies.getModel[CaptureCertificateDetailsFormModel].get
+      val captureCertificateDetails = request.cookies.getModel[CaptureCertificateDetailsModel].get
 
       if (paymentModel.isDefined) {
 
@@ -159,6 +161,8 @@ final class Fulfil @Inject()(
           keeperEmail = request.cookies.getModel[ConfirmFormModel].flatMap(_.keeperEmail),
           businessDetailsModel = request.cookies.getModel[BusinessDetailsModel],
           paymentModel = paymentModel,
+          captureCertificateDetailFormModel = Some(captureCertificateDetailsFormModel),
+          captureCertificateDetailsModel = Some(captureCertificateDetails),
           rejectionCode = Some(responseCode)))
         auditService2.send(AuditRequest.from(
           pageMovement = AuditMessage.PaymentToPaymentFailure,
@@ -168,6 +172,8 @@ final class Fulfil @Inject()(
           keeperEmail = request.cookies.getModel[ConfirmFormModel].flatMap(_.keeperEmail),
           businessDetailsModel = request.cookies.getModel[BusinessDetailsModel],
           paymentModel = paymentModel,
+          captureCertificateDetailFormModel = Some(captureCertificateDetailsFormModel),
+          captureCertificateDetailsModel = Some(captureCertificateDetails),
           rejectionCode = Some(responseCode)))
 
         Redirect(routes.FulfilFailure.present()).
@@ -181,6 +187,8 @@ final class Fulfil @Inject()(
           vehicleAndKeeperDetailsModel = request.cookies.getModel[VehicleAndKeeperDetailsModel],
           keeperEmail = request.cookies.getModel[ConfirmFormModel].flatMap(_.keeperEmail),
           businessDetailsModel = request.cookies.getModel[BusinessDetailsModel],
+          captureCertificateDetailFormModel = Some(captureCertificateDetailsFormModel),
+          captureCertificateDetailsModel = Some(captureCertificateDetails),
           rejectionCode = Some(responseCode)))
         auditService2.send(AuditRequest.from(
           pageMovement = AuditMessage.ConfirmToFulfilFailure,
@@ -189,6 +197,8 @@ final class Fulfil @Inject()(
           vehicleAndKeeperDetailsModel = request.cookies.getModel[VehicleAndKeeperDetailsModel],
           keeperEmail = request.cookies.getModel[ConfirmFormModel].flatMap(_.keeperEmail),
           businessDetailsModel = request.cookies.getModel[BusinessDetailsModel],
+          captureCertificateDetailFormModel = Some(captureCertificateDetailsFormModel),
+          captureCertificateDetailsModel = Some(captureCertificateDetails),
           rejectionCode = Some(responseCode)))
 
         Redirect(routes.FulfilFailure.present()).
