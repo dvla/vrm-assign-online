26d3e75b75ff51b04c2d70c743262c8605bedf74
Chris Rudd <chris.rudd@valtech.co.uk>
2015-03-05 14:46:24 +0000

    US2470: Added keeperChangeDate to vehicleAndKeeperLookup.

--- test/helpers/vrm_assign/CookieFactoryForUnitSpecs.scala
+++ test/helpers/vrm_assign/CookieFactoryForUnitSpecs.scala
@@ -89,6 +89,7 @@ object CookieFactoryForUnitSpecs extends TestComposition {
       address = Some(addressViewModel),
       disposeFlag = None,
       keeperEndDate = None,
+      keeperChangeDate = None,
       suppressedV5Flag = None
     )
     createCookie(key, value)
