1ed01b1de3df271a048c7123d0461b176526bab4
Geraint Jones <geraint.jones@valtech.co.uk>
2015-04-08 14:30:05 +0100

    US2706 Apply two column layout to error pages

--- app/controllers/FulfilFailure.scala
+++ app/controllers/FulfilFailure.scala
@@ -1,12 +1,13 @@
 package controllers
 
 import com.google.inject.Inject
-import models.PaymentModel
+import models._
 import play.api.Logger
 import play.api.mvc.Result
 import play.api.mvc._
 import uk.gov.dvla.vehicles.presentation.common.clientsidesession.ClientSideSessionFactory
 import uk.gov.dvla.vehicles.presentation.common.clientsidesession.CookieImplicits.RichCookies
+import uk.gov.dvla.vehicles.presentation.common.model.VehicleAndKeeperDetailsModel
 import utils.helpers.Config
 import views.vrm_assign.VehicleLookup._
 import webserviceclients.paymentsolve.PaymentSolveCancelRequest
@@ -23,12 +24,20 @@ final class FulfilFailure @Inject()(paymentSolveService: PaymentSolveService)
 
   def present = Action.async { implicit request =>
     (request.cookies.getString(TransactionIdCacheKey),
-      request.cookies.getModel[PaymentModel]) match {
+      request.cookies.getModel[PaymentModel],
+      request.cookies.getModel[VehicleAndKeeperDetailsModel],
+      request.cookies.getModel[VehicleAndKeeperLookupFormModel],
+      request.cookies.getModel[CaptureCertificateDetailsFormModel],
+      request.cookies.getModel[CaptureCertificateDetailsModel]) match {
 
-      case (Some(transactionId), Some(paymentModel)) =>
-        callCancelWebPaymentService(transactionId, paymentModel.trxRef.get, paymentModel.isPrimaryUrl)
-      case (Some(transactionId), None) =>
-        Future.successful(Ok(views.html.vrm_assign.fulfil_failure(false)))
+      case (Some(transactionId), Some(paymentModel), vehicleAndKeeperDetails, Some(vehicleAndKeeperLookupForm), captureCertificateDetailsFormModel, captureCertificateDetailsModel) =>
+        callCancelWebPaymentService(transactionId, paymentModel.trxRef.get, paymentModel.isPrimaryUrl, vehicleAndKeeperDetails, vehicleAndKeeperLookupForm, captureCertificateDetailsFormModel, captureCertificateDetailsModel)
+      case (Some(transactionId), None, vehicleAndKeeperDetails, Some(vehicleAndKeeperLookupForm), captureCertificateDetailsFormModel, captureCertificateDetailsModel) =>
+        val viewModel = vehicleAndKeeperDetails match {
+          case Some(details) => VehicleLookupFailureViewModel(details)
+          case None => VehicleLookupFailureViewModel(vehicleAndKeeperLookupForm)
+        }
+        Future.successful(Ok(views.html.vrm_assign.fulfil_failure(transactionId, false, viewModel, captureCertificateDetailsFormModel, captureCertificateDetailsModel)))
       case _ =>
         Future.successful(Redirect(routes.Error.present("user tried to go to FulfilFailure but the required cookie was not present")))
     }
@@ -37,8 +46,11 @@ final class FulfilFailure @Inject()(paymentSolveService: PaymentSolveService)
   private def callCancelWebPaymentService(
                                            transactionId: String,
                                            trxRef: String,
-                                           isPrimaryUrl: Boolean
-                                           )
+                                           isPrimaryUrl: Boolean,
+                                           vehicleAndKeeperDetails: Option[VehicleAndKeeperDetailsModel],
+                                           vehicleAndKeeperLookupForm: VehicleAndKeeperLookupFormModel,
+                                           captureCertificateDetailsFormModel: Option[CaptureCertificateDetailsFormModel],
+                                           captureCertificateDetailsModel: Option[CaptureCertificateDetailsModel])
                                          (implicit request: Request[_]): Future[Result] = {
 
     val paymentSolveCancelRequest = PaymentSolveCancelRequest(
@@ -48,13 +60,18 @@ final class FulfilFailure @Inject()(paymentSolveService: PaymentSolveService)
     )
     val trackingId = request.cookies.trackingId()
 
+    val viewModel = vehicleAndKeeperDetails match {
+      case Some(details) => VehicleLookupFailureViewModel(details)
+      case None => VehicleLookupFailureViewModel(vehicleAndKeeperLookupForm)
+    }
+
     paymentSolveService.invoke(paymentSolveCancelRequest, trackingId).map {
       response =>
-        Ok(views.html.vrm_assign.fulfil_failure(true))
+        Ok(views.html.vrm_assign.fulfil_failure(transactionId, true, viewModel, captureCertificateDetailsFormModel, captureCertificateDetailsModel))
     }.recover {
       case NonFatal(e) =>
         Logger.error(s"FulfilFailure Payment Solve web service call with paymentSolveCancelRequest failed. Exception " + e.toString)
-        Ok(views.html.vrm_assign.fulfil_failure(true))
+        Ok(views.html.vrm_assign.fulfil_failure(transactionId, true, viewModel, captureCertificateDetailsFormModel, captureCertificateDetailsModel))
     }
   }
 }
