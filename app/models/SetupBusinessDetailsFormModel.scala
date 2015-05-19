14115b8a12c7ac9255c6464a34e82a05e7b09db9
Luchesar Cekov <luchesar@gmail.com>
2015-05-19 13:25:10 +0100

    US2860 adapt to the latest AdressPicker in common

--- app/models/SetupBusinessDetailsFormModel.scala
+++ app/models/SetupBusinessDetailsFormModel.scala
@@ -7,6 +7,7 @@ import uk.gov.dvla.vehicles.presentation.common.clientsidesession.CacheKey
 import uk.gov.dvla.vehicles.presentation.common.mappings.{AddressPicker, BusinessName}
 import uk.gov.dvla.vehicles.presentation.common.mappings.Email.emailConfirm
 import uk.gov.dvla.vehicles.presentation.common.mappings.Postcode.postcode
+import uk.gov.dvla.vehicles.presentation.common.model.SearchFields
 import uk.gov.dvla.vehicles.presentation.common.model.Address
 import views.vrm_assign.SetupBusinessDetails.BusinessContactId
 import views.vrm_assign.SetupBusinessDetails.BusinessEmailId
@@ -17,8 +18,8 @@ import views.vrm_assign.SetupBusinessDetails.SetupBusinessDetailsCacheKey
 final case class SetupBusinessDetailsFormModel(name: String, contact: String, email: String, address: Address)
 
 object SetupBusinessDetailsFormModel {
-
-  implicit val addressFromat = Json.format[Address]
+  implicit val searchFieldsFormat = Json.format[SearchFields]
+  implicit val addressFormat = Json.format[Address]
   implicit val JsonFormat = Json.format[SetupBusinessDetailsFormModel]
   implicit val Key = CacheKey[SetupBusinessDetailsFormModel](SetupBusinessDetailsCacheKey)
 
