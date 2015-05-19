867d41a37390d60634632621d3b8908618b17e72
Gerasimos.Arvanitis <gerasimos.arvanitis@valtech.co.uk>
2015-05-19 15:23:33 +0100

    US3113: put timeout on its own route

--- app/controllers/Fulfil.scala
+++ app/controllers/Fulfil.scala
@@ -34,8 +34,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.util.control.NonFatal
 
-final class Fulfil @Inject()(
-                              vrmAssignFulfilService: VrmAssignFulfilService,
+final class Fulfil @Inject()(vrmAssignFulfilService: VrmAssignFulfilService,
                               auditService2: audit2.AuditService
                               )
                             (implicit clientSideSessionFactory: ClientSideSessionFactory,
@@ -207,10 +206,7 @@ final class Fulfil @Inject()(
             }
         }
     }.recover {
-      case _: TimeoutException => {
-        val vehicleAndKeeperDetailsModel = request.cookies.getModel[VehicleAndKeeperDetailsModel].get
-        Ok(views.html.vrm_assign.timeout(vehicleAndKeeperDetailsModel))
-      }
+      case _: TimeoutException =>  Redirect(routes.TimeoutController.present())
       case NonFatal(e) =>
         microServiceErrorResult(s"VRM Assign Fulfil web service call failed. Exception " + e.toString)
     }
---
+++ app/controllers/TimeoutController.scala
@@ -0,0 +1,33 @@
+package controllers
+
+import com.google.inject.Inject
+import play.api.mvc.{Action, Controller}
+import models.CacheKeyPrefix
+import uk.gov.dvla.vehicles.presentation.common.clientsidesession.ClientSideSessionFactory
+import uk.gov.dvla.vehicles.presentation.common.clientsidesession.CookieImplicits.RichCookies
+import uk.gov.dvla.vehicles.presentation.common.model.VehicleAndKeeperDetailsModel
+import utils.helpers.Config
+import views.vrm_assign.Confirm._
+
+/**
+ * Shows a timeout page to inform the end user when the fulfil service timeouts.
+ */
+class TimeoutController @Inject()()(implicit clientSideSessionFactory: ClientSideSessionFactory,
+                          config: Config,
+                          dateService: uk.gov.dvla.vehicles.presentation.common.services.DateService) extends Controller {
+
+  /**
+   * presents the end use with the timeout page.
+   * We check that the user went through the confirm page at least before we show this page.
+   * @return the timeout view or a redirect to the start in the case of error.
+   */
+  def present = Action { implicit request =>
+    (for {
+      _  <- request.cookies.getString(GranteeConsentCacheKey)
+    } yield {
+      val vehicleDetails = request.cookies.getModel[VehicleAndKeeperDetailsModel]
+      Ok(views.html.vrm_assign.timeout(vehicleDetails))
+    }) getOrElse Redirect(routes.VehicleLookup.present())
+  }
+
+}
