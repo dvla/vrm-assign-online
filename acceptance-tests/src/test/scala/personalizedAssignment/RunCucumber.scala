0ca1a7ed7caa05e27b11d936f15b838ec43152cb
Luchesar Cekov <luchesar@gmail.com>
2015-05-20 15:50:59 +0100

    US2860 all the cucumber tests are passing

--- acceptance-tests/src/test/scala/personalizedAssignment/RunCucumber.scala
+++ acceptance-tests/src/test/scala/personalizedAssignment/RunCucumber.scala
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith
 
 @RunWith(classOf[Cucumber])
 @CucumberOptions(
-  features = Array("acceptance-tests/src/test/resources/personalizedAssignment/VehiclePersonalAssignment.feature"),
+  features = Array("acceptance-tests/src/test/resources/personalizedAssignment"),
   //  features = Array("acceptance-tests/src/test/resources/personalizedAssignment/browser"),
   glue = Array("personalizedAssignment.stepDefs"),
   //  plugin = Array("pretty", "html:target/cucumber-report"),
