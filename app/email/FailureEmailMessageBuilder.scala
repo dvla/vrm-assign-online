a37b9b83e93d433ab1a665e663d0201cb4a3aef9
Chris Rudd <chris.rudd@valtech.co.uk>
2015-06-19 11:23:42 +0100

    US3112: Updated messages file for failure emai.

--- app/email/FailureEmailMessageBuilder.scala
+++ app/email/FailureEmailMessageBuilder.scala
@@ -3,6 +3,7 @@ package email
 import java.text.SimpleDateFormat
 
 import org.joda.time.{DateTimeZone, Instant}
+import play.api.i18n.Messages
 
 /**
  * The email message builder class will create the contents of the message. override the buildHtml and buildText
@@ -28,6 +29,43 @@ object FailureEmailMessageBuilder {
     )
   }
 
+//  private def buildHtml: String =
+//    s"""
+//       |<html>
+//       |<head>
+//       |</head>
+//       |<style>
+//       |p {
+//       |  line-height: 200%;
+//       |}
+//       |ul { list-style: none; padding: 0; margin:0 0 32px 0;}
+//       |li { margin-bottom: 8px}
+//       |li > ul {
+//       |	margin: 16px 0 0 16px;
+//       |}
+//       |</style>
+//       |</head>
+//       |<body>
+//       |
+//       |<p>
+//       |	<strong>THIS IS AN AUTOMATED EMAIL - PLEASE DO NOT REPLY.</strong>
+//       |</p>
+//       |
+//       |<p>Payment received.</p>
+//       |
+//        |<p><i>DVLA, Swansea, SA6 7JL</i></p>
+//      """.stripMargin
+//
+//  private def buildText: String =
+//    s"""
+//       |THIS IS AN AUTOMATED EMAIL - PLEASE DO NOT REPLY.
+//       |
+//       |
+//        |
+//        |DVLA, Swansea, SA6 7JL
+//        |
+//      """.stripMargin
+
   private def buildHtml: String =
     s"""
        |<html>
@@ -47,22 +85,47 @@ object FailureEmailMessageBuilder {
        |<body>
        |
        |<p>
-       |	<strong>THIS IS AN AUTOMATED EMAIL - PLEASE DO NOT REPLY.</strong>
-       |</p>
-       |
-       |<p>Payment received.</p>
-       |
-        |<p><i>DVLA, Swansea, SA6 7JL</i></p>
-      """.stripMargin
+       |	<strong>
+       |  ${Messages("email.failure.line1")}
+        |  ${Messages("email.failure.line2")}
+        | </strong>
+        |</p>
+        |
+        |<p>
+        |  ${Messages("email.failure.line3")}
+        |  ${Messages("email.failure.line4")}
+        |  ${Messages("email.failure.line5")}
+        |  ${Messages("email.failure.line6")}
+        |  ${Messages("email.failure.line7")}
+        |  ${Messages("email.failure.line8")}
+        |  ${Messages("email.failure.line9")}
+        |  ${Messages("email.failure.line10")}
+        |  ${Messages("email.failure.line11")}
+        |  ${Messages("email.failure.line12")}
+        |  ${Messages("email.failure.line14")}
+        |  ${Messages("email.failure.line15")}
+        |  ${Messages("email.failure.line16")}
+        |</p>
+        """.stripMargin
 
   private def buildText: String =
     s"""
-       |THIS IS AN AUTOMATED EMAIL - PLEASE DO NOT REPLY.
-       |
-       |
-        |
-        |DVLA, Swansea, SA6 7JL
-        |
-      """.stripMargin
+       |${Messages("email.failure.line1")}
+        |${Messages("email.failure.line2")}
+        |${Messages("email.failure.line3")}
+        |${Messages("email.failure.line4")}
+        |${Messages("email.failure.line5")}
+        |${Messages("email.failure.line6")}
+        |${Messages("email.failure.line7")}
+        |${Messages("email.failure.line8")}
+        |${Messages("email.failure.line9")}
+        |${Messages("email.failure.line10")}
+        |${Messages("email.failure.line11")}
+        |${Messages("email.failure.line12")}
+        |${Messages("email.failure.line13")}
+        |${Messages("email.failure.line14")}
+        |${Messages("email.failure.line15")}
+        |${Messages("email.failure.line16")}
+        """.stripMargin
 
 }
