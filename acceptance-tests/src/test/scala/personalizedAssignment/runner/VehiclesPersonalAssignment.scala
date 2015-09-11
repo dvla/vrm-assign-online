4ac0dcee0c0c2a20a68023395a11635039db2189
Heitham Omar <Heitham.Omar@dvla.gsi.gov.uk>
2015-09-11 14:12:23 +0100

    US2870 Speed up the build and the running of acceptance tests

---
+++ acceptance-tests/src/test/scala/personalizedAssignment/runner/VehiclesPersonalAssignment.scala
@@ -0,0 +1,13 @@
+package PersonalizedRegistration.runner
+
+import cucumber.api.CucumberOptions
+import cucumber.api.junit.Cucumber
+import org.junit.runner.RunWith
+
+@RunWith(classOf[Cucumber])
+@CucumberOptions(
+  features = Array("acceptance-tests/src/test/resources/personalizedAssignment/VehiclePersonalAssignment.feature"),
+  glue = Array("personalizedAssignment.stepDefs"),
+  tags = Array("~@WIP", "~@browser", "~@live-payment")
+)
+class VehiclePersonalAssignment
