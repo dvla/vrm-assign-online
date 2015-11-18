629b8cfb0580cc92845d4a5d602271204277847d
Heitham Omar <Heitham.Omar@dvla.gsi.gov.uk>
2015-11-18 13:46:53 +0000

    US3977 Create a CookiePolicy controller in vehicles-presentation-common

--- app/controllers/CookiePolicy.scala
+++ app/controllers/CookiePolicy.scala
@@ -7,10 +7,10 @@ import utils.helpers.Config
 
 final class CookiePolicy @Inject()()(implicit clientSideSessionFactory: ClientSideSessionFactory,
                                      config: Config,
-                                     dateService: uk.gov.dvla.vehicles.presentation.common.services.DateService
-                                    ) extends Controller {
+                                     dateService: uk.gov.dvla.vehicles.presentation.common.services.DateService)
+                                     extends uk.gov.dvla.vehicles.presentation.common.controllers.CookiePolicy {
 
   def present = Action { implicit request =>
-    Ok(views.html.vrm_assign.cookie_policy())
+    Ok(views.html.vrm_assign.cookie_policy(cookies))
   }
 }
