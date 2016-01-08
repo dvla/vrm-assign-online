5b06e02631ad7562bbe28c4101b8db3e4e832e34
Ian Stainer <ian.stainer@valtech.co.uk>
2016-01-08 15:52:43 +0000

    US4176 Verify that all micro-services called by the assign web app inform the HealthStats service of success or failure

--- app/webserviceclients/vrmassigneligibility/VrmAssignEligibilityServiceImpl.scala
+++ app/webserviceclients/vrmassigneligibility/VrmAssignEligibilityServiceImpl.scala
@@ -6,23 +6,47 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.util.control.NonFatal
 import uk.gov.dvla.vehicles.presentation.common.clientsidesession.TrackingId
+import uk.gov.dvla.vehicles.presentation.common.services.DateService
+import uk.gov.dvla.vehicles.presentation.common.webserviceclients.healthstats.HealthStats
+import uk.gov.dvla.vehicles.presentation.common.webserviceclients.healthstats.HealthStatsFailure
+import uk.gov.dvla.vehicles.presentation.common.webserviceclients.healthstats.HealthStatsSuccess
 
-final class VrmAssignEligibilityServiceImpl @Inject()(ws: VrmAssignEligibilityWebService)
+final class VrmAssignEligibilityServiceImpl @Inject()(ws: VrmAssignEligibilityWebService,
+                                                      dateService: DateService,
+                                                      healthStats: HealthStats)
   extends VrmAssignEligibilityService {
 
   override def invoke(cmd: VrmAssignEligibilityRequest,
                       trackingId: TrackingId)
-                    : Future[(Int, VrmAssignEligibilityResponseDto)] =
+                    : Future[(Int, VrmAssignEligibilityResponseDto)] = {
+    import VrmAssignEligibilityServiceImpl.ServiceName
     ws.invoke(cmd, trackingId).map { resp =>
-      if (resp.status == Status.OK)
+      if (resp.status == Status.OK) {
+        healthStats.success(HealthStatsSuccess(ServiceName, dateService.now))
         (resp.status, resp.json.as[VrmAssignEligibilityResponseDto])
-      else if (resp.status == Status.INTERNAL_SERVER_ERROR)
+      }
+      else if (resp.status == Status.INTERNAL_SERVER_ERROR) {
+        val msg = s"Vrm Assign Eligibility micro-service call http status not OK, it was: ${resp.status}"
+        val error = new RuntimeException(msg)
+        healthStats.failure(HealthStatsFailure(ServiceName, dateService.now, error))
         (resp.status, resp.json.as[VrmAssignEligibilityResponseDto])
-      else throw new RuntimeException(
-        "Vrm Assign Eligibility web service call http status not OK, it " +
-          s"was: ${resp.status}. Problem may come from either vrm-assign-eligibility micro-service or the VSS"
-      )
+      }
+      else {
+        val error = new RuntimeException(
+          "Vrm Assign Eligibility micro-service call http status not OK, it " +
+            s"was: ${resp.status}. Problem may come from either vrm-assign-eligibility micro-service or VSS"
+        )
+        healthStats.failure(HealthStatsFailure(ServiceName, dateService.now, error))
+        throw error
+      }
     }.recover {
-      case NonFatal(e) => throw e
+      case NonFatal(e) =>
+        healthStats.failure(HealthStatsFailure(ServiceName, dateService.now, e))
+        throw e
     }
+  }
+}
+
+object VrmAssignEligibilityServiceImpl {
+  final val ServiceName = "vrm-assign-eligibility-microservice"
 }
--- app/webserviceclients/vrmassignfulfil/VrmAssignFulfilServiceImpl.scala
+++ app/webserviceclients/vrmassignfulfil/VrmAssignFulfilServiceImpl.scala
@@ -5,20 +5,46 @@ import play.api.http.Status
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import uk.gov.dvla.vehicles.presentation.common.clientsidesession.TrackingId
+import uk.gov.dvla.vehicles.presentation.common.services.DateService
+import uk.gov.dvla.vehicles.presentation.common.webserviceclients.healthstats.HealthStats
+import uk.gov.dvla.vehicles.presentation.common.webserviceclients.healthstats.HealthStatsFailure
+import uk.gov.dvla.vehicles.presentation.common.webserviceclients.healthstats.HealthStatsSuccess
 
-final class VrmAssignFulfilServiceImpl @Inject()(ws: VrmAssignFulfilWebService) extends VrmAssignFulfilService {
+import scala.util.control.NonFatal
 
-  override def invoke(cmd: VrmAssignFulfilRequest, trackingId: TrackingId):
-                                                                          Future[(Int, VrmAssignFulfilResponseDto)] = {
+final class VrmAssignFulfilServiceImpl @Inject()(ws: VrmAssignFulfilWebService,
+                                                 dateService: DateService,
+                                                 healthStats: HealthStats) extends VrmAssignFulfilService {
+
+  override def invoke(cmd: VrmAssignFulfilRequest, trackingId: TrackingId): Future[(Int, VrmAssignFulfilResponseDto)] = {
+    import VrmAssignFulfilServiceImpl.ServiceName
     ws.invoke(cmd, trackingId).map { resp =>
-      if (resp.status == Status.OK)
+      if (resp.status == Status.OK) {
+        healthStats.success(HealthStatsSuccess(ServiceName, dateService.now))
         (resp.status, resp.json.as[VrmAssignFulfilResponseDto])
-      else if (resp.status == Status.INTERNAL_SERVER_ERROR)
+      }
+      else if (resp.status == Status.INTERNAL_SERVER_ERROR) {
+        val msg = s"Vrm Assign Fulfil micro-service call http status not OK, it was: ${resp.status}"
+        val error = new RuntimeException(msg)
+        healthStats.failure(HealthStatsFailure(ServiceName, dateService.now, error))
         (resp.status, resp.json.as[VrmAssignFulfilResponseDto])
-      else throw new RuntimeException(
-        "Vrm Assign Fulfil web service call http status not OK, it " +
-          s"was: ${resp.status}. Problem may come from either vrm-assign-fulfil micro-service or the VSS"
-      )
+      }
+      else {
+        val error = new RuntimeException(
+          "Vrm Assign Fulfil micro service call http status not OK, it " +
+            s"was: ${resp.status}. Problem may come from either vrm-assign-fulfil micro-service or VSS"
+        )
+        healthStats.failure(HealthStatsFailure(ServiceName, dateService.now, error))
+        throw error
+      }
+    }.recover {
+      case NonFatal(e) =>
+        healthStats.failure(HealthStatsFailure(ServiceName, dateService.now, e))
+        throw e
     }
   }
+}
+
+object VrmAssignFulfilServiceImpl {
+  final val ServiceName = "vrm-assign-fulfil-microservice"
 }
