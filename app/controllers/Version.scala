9059bb9b086489f38c81bfe438eeeac94d3c6c30
Ian Stainer <ian.stainer@valtech.co.uk>
2016-08-12 12:15:43 +0100

    VMPRCI-4490 updated version information so we now include version information on all dependent micro services displayed alphabetically

--- app/controllers/Version.scala
+++ app/controllers/Version.scala
@@ -2,19 +2,19 @@ package controllers
 
 import com.google.inject.Inject
 import uk.gov.dvla.vehicles.presentation.common.controllers
+import uk.gov.dvla.vehicles.presentation.common.controllers.Version.Suffix
 import uk.gov.dvla.vehicles.presentation.common.webserviceclients.addresslookup.ordnanceservey.OrdnanceSurveyConfig
 import uk.gov.dvla.vehicles.presentation.common.webserviceclients.vehicleandkeeperlookup.VehicleAndKeeperLookupConfig
 import utils.helpers.Config
 
-class Version @Inject()(vehiclesKeeperConfig: VehicleAndKeeperLookupConfig,
+class Version @Inject()(vehicleAndKeeperConfig: VehicleAndKeeperLookupConfig,
                         osAddressLookupConfig: OrdnanceSurveyConfig,
-                        config: Config)
-  extends controllers.Version(
-    osAddressLookupConfig.baseUrl + "/version",
-    vehiclesKeeperConfig.vehicleAndKeeperLookupMicroServiceBaseUrl + "/version",
-    config.emailServiceMicroServiceUrlBase + "/version",
-    config.paymentSolveMicroServiceUrlBase + "/version",
-    config.vrmAssignEligibilityMicroServiceUrlBase + "/version",
-    config.vrmAssignFulfilMicroServiceUrlBase + "/version",
-    config.auditMicroServiceUrlBase + "/version"
-  )
+                        config: Config) extends controllers.Version(
+    config.auditMicroServiceUrlBase + Suffix,
+    config.emailServiceMicroServiceUrlBase + Suffix,
+    osAddressLookupConfig.baseUrl + Suffix,
+    config.paymentSolveMicroServiceUrlBase + Suffix,
+    vehicleAndKeeperConfig.vehicleAndKeeperLookupMicroServiceBaseUrl + Suffix,
+    config.vrmAssignEligibilityMicroServiceUrlBase + Suffix,
+    config.vrmAssignFulfilMicroServiceUrlBase + Suffix
+)
