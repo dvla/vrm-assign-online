d543191f87d00e7777ae2be0c9faf2503fa7e250
Heitham Omar <Heitham.Omar@dvla.gsi.gov.uk>
2016-08-19 12:42:49 +0100

    VMPRCI-4288 Update VMPR Cookies policy

--- app/controllers/CookiePolicy.scala
+++ app/controllers/CookiePolicy.scala
@@ -2,7 +2,9 @@ package controllers
 
 import com.google.inject.Inject
 import play.api.mvc.{Action, Controller}
-import uk.gov.dvla.vehicles.presentation.common.clientsidesession.ClientSideSessionFactory
+import uk.gov.dvla.vehicles.presentation.common
+import common.clientsidesession.ClientSideSessionFactory
+import common.model.CookieReport
 import utils.helpers.Config
 
 final class CookiePolicy @Inject()()(implicit clientSideSessionFactory: ClientSideSessionFactory,
@@ -10,7 +12,17 @@ final class CookiePolicy @Inject()()(implicit clientSideSessionFactory: ClientSi
                                      dateService: uk.gov.dvla.vehicles.presentation.common.services.DateService)
                                      extends uk.gov.dvla.vehicles.presentation.common.controllers.CookiePolicy {
 
+  private val reports = cookies ++ List(
+    CookieReport("tracking_id", "tracking_id", "normal", "7days"),
+    CookieReport("PLAY_LANG", "PLAY_LANG", "session", "close"),
+    CookieReport("40 character length", "multi-encrypted", "normal-secure", "7daysOr8hours"),
+    CookieReport("liveagent_oref", "chat-support", "normal", "10years"),
+    CookieReport("liveagent_vc", "chat-support", "normal", "10years"),
+    CookieReport("liveagent_ptid", "chat-support", "normal", "10years"),
+    CookieReport("liveagent_sid", "chat-support", "session", "close")
+  )
+
   def present = Action { implicit request =>
-    Ok(views.html.vrm_assign.cookie_policy(cookies))
+    Ok(views.html.vrm_assign.cookie_policy(reports))
   }
 }
