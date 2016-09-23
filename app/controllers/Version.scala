32eeb09ffd154bd92faae5d6c3ecadde8619c6cf
Ian Stainer <ian.stainer@valtech.co.uk>
2016-09-23 10:33:25 +0100

    VMPRCI-4513 rolled back temp fix

--- app/controllers/Version.scala
+++ app/controllers/Version.scala
@@ -7,14 +7,14 @@ import uk.gov.dvla.vehicles.presentation.common.webserviceclients.addresslookup.
 import uk.gov.dvla.vehicles.presentation.common.webserviceclients.vehicleandkeeperlookup.VehicleAndKeeperLookupConfig
 import utils.helpers.Config
 
-class Version @Inject()(//vehicleAndKeeperConfig: VehicleAndKeeperLookupConfig,
-//                        osAddressLookupConfig: OrdnanceSurveyConfig,
+class Version @Inject()(vehicleAndKeeperConfig: VehicleAndKeeperLookupConfig,
+                        osAddressLookupConfig: OrdnanceSurveyConfig,
                         config: Config) extends controllers.Version(
     config.auditMicroServiceUrlBase + Suffix,
     config.emailServiceMicroServiceUrlBase + Suffix,
-//    osAddressLookupConfig.baseUrl + Suffix,
+    osAddressLookupConfig.baseUrl + Suffix,
     config.paymentSolveMicroServiceUrlBase + Suffix,
-//    vehicleAndKeeperConfig.vehicleAndKeeperLookupMicroServiceBaseUrl + Suffix,
+    vehicleAndKeeperConfig.vehicleAndKeeperLookupMicroServiceBaseUrl + Suffix,
     config.vrmAssignEligibilityMicroServiceUrlBase + Suffix,
     config.vrmAssignFulfilMicroServiceUrlBase + Suffix
 )
