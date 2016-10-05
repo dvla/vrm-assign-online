10455a0df9e85a46132a9c45d9c3a864046c1d0b
Heitham Omar <Heitham.Omar@dvla.gsi.gov.uk>
2016-10-05 13:51:39 +0100

    VMPRCI-4532 Fix coverage reporting jobs

--- app/views/vrm_assign/RelatedCacheKeys.scala
+++ app/views/vrm_assign/RelatedCacheKeys.scala
@@ -52,7 +52,7 @@ object RelatedCacheKeys {
     StoreBusinessDetailsCacheKey
   )
 
-  def removeCookiesOnExit(implicit request: Request[_], clientSideSessionFactory: ClientSideSessionFactory) = {
+  def removeCookiesOnExit()(implicit request: Request[_], clientSideSessionFactory: ClientSideSessionFactory) = {
     val storeBusinessDetails = request.cookies.getString(StoreBusinessDetailsCacheKey).exists(_.toBoolean)
     RelatedCacheKeys.AssignSet ++ {
       if (storeBusinessDetails) Set.empty else RelatedCacheKeys.BusinessDetailsSet
